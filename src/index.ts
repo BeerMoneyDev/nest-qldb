b7b7389e8be818551d3df672d3470dcf3da20a8e
Kerry Ritter <ritter@kerryritter.com>
2020-08-14 19:30:51 -0500

    feature(QueryService): added new QueryService provider

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { QldbTable, InjectRepository } from './decorators';
 export { Repository } from './repository';
 export { NestQldbModule } from './nest-qldb.module';
 export { QldbDriver } from 'amazon-qldb-driver-nodejs';
+export * from './query.service';
 export * from './query';
---
+++ src/query.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { QldbDriver, Result } from 'amazon-qldb-driver-nodejs';
+import { QLDB_DRIVER_TOKEN } from './tokens';
+
+@Injectable()
+export class QldbQueryService {
+  constructor(
+    @Inject(QLDB_DRIVER_TOKEN)
+    private readonly driver: QldbDriver,
+  ) {}
+
+  async query<T>(statement: string, ...parameters: any[]): Promise<T[]> {
+    const result = await this.execute(statement, ...parameters);
+    console.log({ result });
+    return this.mapResultsToObjects<T>(result);
+  }
+
+  async querySingle<T>(statement: string, ...parameters: any[]): Promise<T> {
+    const result = await this.execute(statement, ...parameters);
+
+    return this.mapResultsToObjects<T>(result)?.[0];
+  }
+
+  async queryForSubdocument<T>(
+    statement: string,
+    subproperty: string,
+    ...parameters: any[]
+  ): Promise<T[]> {
+    const result = await this.execute(statement, ...parameters);
+
+    return this.mapResultsToObjects<T>(result, subproperty);
+  }
+
+  async querySingleForSubdocument<T>(
+    statement: string,
+    subproperty: string,
+    ...parameters: any[]
+  ): Promise<T> {
+    const result = await this.execute(statement, ...parameters);
+
+    return this.mapResultsToObjects<T>(result, subproperty)?.[0];
+  }
+
+  async execute(statement: string, ...parameters: any[]): Promise<Result> {
+    return await this.driver.executeLambda(
+      async txn => await txn.execute(statement, ...parameters),
+    );
+  }
+
+  private mapResultsToObjects<T>(result: Result, subproperty?: string): T[] {
+    if (!result) {
+      return null;
+    }
+
+    const resultList = result.getResultList();
+
+    if (!resultList?.length) {
+      return null;
+    }
+
+    return resultList.map(value => {
+      const parsedJson = JSON.parse(JSON.stringify(value));
+
+      return subproperty ? parsedJson[subproperty] : parsedJson;
+    });
+  }
+}
