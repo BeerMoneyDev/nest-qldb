a2721a7b17a595399be9cddb3e79627c60891274
Ronnie Hicks <hicks.ronnie@gmail.com>
2021-03-03 14:32:34 -0600

    fix(Module): Fixing module registration issues

--- test/factory.e2e-spec.ts
+++ test/factory.e2e-spec.ts
@@ -1,20 +1,12 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { SharedIniFileCredentials } from 'aws-sdk';
 import {
-  InjectRepository,
   NestQldbModule,
   QldbDriver,
   QldbQueryServiceFactoryService,
-  QldbTable,
-  Repository,
 } from '../src';
 
 /* USERS */
-@QldbTable({
-  tableName: 'app_users',
-  tableIndexes: ['dob', 'sex'],
-})
 class User {
   dob: Date;
   name: string;
@@ -27,42 +19,33 @@ class User {
     Object.assign(this, partial);
   }
 }
-
-@Injectable()
-class UserService {
-  constructor(
-    @InjectRepository(User) readonly usersRepository: Repository<User>,
-  ) {}
-}
 /* END USERS */
 
 // Setup Module
 const driver = new QldbDriver('test-ledger', {
   region: 'us-east-1',
-  credentials: new SharedIniFileCredentials({
-    profile: 'test-profile',
-  }),
+  // credentials: new SharedIniFileCredentials({
+  //   profile: 'test-profile',
+  // }),
 });
 @Module({
-  imports: [
-    NestQldbModule.forRoot({
-      qldbDriver: driver,
-      createTablesAndIndexes: true,
-      tables: [User],
-    }),
-  ],
-  providers: [UserService],
-  exports: [UserService],
+  imports: [NestQldbModule.forRoot({})],
 })
 class TestRootModule {}
+
+@Module({
+  imports: [NestQldbModule.forRootAsync({})],
+})
+class TestRootAsyncModule {}
 
 describe('NestQldbModule.forRoot()', () => {
   it('should work', async () => {
     jest.setTimeout(30000);
 
     const module = await NestFactory.createApplicationContext(TestRootModule);
+    const factoryService = module.get(QldbQueryServiceFactoryService);
+    const queryService = factoryService.create(driver);
 
-    const userService = module.get(UserService);
     const name = 'Billy Madison';
     const luckyNumber = 69;
     const user = new User({
@@ -75,11 +58,59 @@ describe('NestQldbModule.forRoot()', () => {
     });
 
     // create
-    const created = await userService.usersRepository.create(user);
+    try {
+      await queryService.execute('CREATE TABLE app_users');
+    } catch {
+      // table already exists, throw away this error
+    }
+    await queryService.execute('INSERT INTO app_users ?', user);
 
     // custom query
+    const result = await queryService.query<User>(
+      `
+      SELECT a.*
+      FROM app_users AS a,
+          a.groups AS m
+      WHERE m.name = ?
+    `,
+      'best_golfers_ever',
+    );
+    expect(result.find(r => r.name === 'Billy Madison')).toBeDefined();
+
+    await queryService.execute('DROP TABLE app_users');
+  });
+});
+
+describe('NestQldbModule.forRootAsync()', () => {
+  it('should work', async () => {
+    jest.setTimeout(30000);
+
+    const module = await NestFactory.createApplicationContext(
+      TestRootAsyncModule,
+    );
     const factoryService = module.get(QldbQueryServiceFactoryService);
     const queryService = factoryService.create(driver);
+
+    const name = 'Billy Madison';
+    const luckyNumber = 69;
+    const user = new User({
+      dob: new Date('1980/07/19'),
+      name,
+      gender: 'man',
+      sex: 'M',
+      luckyNumber,
+      groups: [{ name: 'best_golfers_ever' }],
+    });
+
+    // create
+    try {
+      await queryService.execute('CREATE TABLE app_users');
+    } catch {
+      // table already exists, throw away this error
+    }
+    await queryService.execute('INSERT INTO app_users ?', user);
+
+    // custom query
     const result = await queryService.query<User>(
       `
       SELECT a.*
